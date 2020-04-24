493c30504168f5c29a7cfc71bc122ebe9ca1f9b6
ndilsou <nasoungadoy@gmail.com>
2020-04-24 01:10:55 +0100

    add the simple monte carlo pricer

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod random;
+pub mod simple_mc;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/random.rs
@@ -0,0 +1,28 @@
+extern crate rand;
+use rand::prelude::*;
+
+pub fn get_one_gaussion_by_box_muller() -> f64 {
+    let mut x: f64;
+    let mut y: f64;
+
+    let mut rng = thread_rng();
+    let mut size_squared: f64;
+    loop  {
+        x = 2.0 * rng.gen::<f64>() - 1.0;
+        y = 2.0 * rng.gen::<f64>() - 1.0;
+        size_squared = x * x + y * y;
+        if size_squared < 1.0 {
+            break;
+        }
+    }
+
+    let result = x * (-2.0 * size_squared.ln() / size_squared).sqrt();
+    result
+
+}
+
+pub fn get_one_gaussian_by_summation() -> f64 {
+    let mut rng = thread_rng();
+    let summation: f64 = (0..12).map(|_| rng.gen::<f64>()).sum();
+    summation - 6.0
+}
